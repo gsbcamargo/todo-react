c076d69ff09970f8f7290e62a7cb25d140a4ac16
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-05-13 01:00:13 -0300

    creates form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import Button from './components/button';
+import Button from './components/Button';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
+      <Form></Form>
       <Button/>
     </div>
   );
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "../Button";
+
+class Form extends React.Component {
+    render() {
+        return (
+            <form>
+                <div>
+                    <label htmlFor="task">
+                        Please add a new study subject:
+                    </label>
+                    <input
+                       type="text"
+                       name="task"
+                       id="task"
+                       placeholder="What do you want to study?"
+                       required
+                     />
+
+                </div>
+
+                <div>   
+                    <label htmlFor="duration">
+                         Duration:
+                    </label>
+                    <input 
+                        type="time"
+                        step="1"
+                        name="duration"
+                        id="duration"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required
+                    />
+                </div>
+                <Button />
+            </form>
+        )
+    }
+}
+
+export default Form;
